3d9a8e50232a4d2063248ca1f4a5cf79bc4531cb
egan-ombaka <eganombaka97@gmail.com>
2025-02-17 17:27:42 +0300

    About section

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section">
+      <div className="about-container">
+        <h4 className="about"> About </h4>
+
+        <p className="about-des">
+          Even though I don't come from a programming background, I learned
+          coding from all the available resources on the internet, and that's
+          where I developed a passion for sharing by building my own YouTube
+          channel, canddev. There are so many kind-hearted people in the
+          programming world, and I aspire to be one of them.
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
